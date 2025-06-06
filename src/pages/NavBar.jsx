2d8fd53b1e9356fda412f0bea8027e7ac4c57369
alessiotinaglia <alessio.tina1998@gmail.com>
2025-06-06 14:56:49 +0200

    navbar completata

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,12 +1,26 @@
-
-
+import styles from './NavBar.module.css';
 
 function NavBar() {
   return (
-    <div>
-      <h1>Nav Bar</h1>      
+    <div className={styles.navbar}>
+      {/* immagine sinistra */}
+      <img src="./aaaa.jpg" alt="Logo sinistro" className={`${styles.logo} ${styles.logoFlipped}`} />
+
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>
+          Tutti i mezzi di trasporto di cui hai bisogno puoi trovarli qui
+        </h1>
+        <p className={styles.subtitle}>
+          Qui puoi trovare auto, moto e molto altro...
+        </p>
+      </div>
+
+      {/* immagine destra */}
+      <img src="./aaaa.jpg" alt="Logo sinistro" className={styles.logo}
+
+      />
     </div>
-  )
+  );
 }
 
 export default NavBar;
